2f12a23f397178fd891a76d06c1ea30b4c86f855
fredy0314 <fredyloaiza6@gmail.com>
2016-07-21 18:57:47 -0500

    comit inicial

---
+++ db/migrate/20160714224946_create_slots.rb
@@ -0,0 +1,11 @@
+class CreateSlots < ActiveRecord::Migration
+  def change
+    create_table :slots do |t|
+      t.integer :location_id
+      t.references :location, index: true, foreign_key: true
+      t.boolean :occupied
+
+      t.timestamps null: false
+    end
+  end
+end
